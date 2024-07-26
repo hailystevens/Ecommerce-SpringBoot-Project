c0bb3a9ff35d78f41e02627d9a11b1f09fdfee65
Haily S <Hailybellestevens@gmail.com>
2024-07-26 10:19:32 -0400

    Implemented users

---
+++ src/main/java/com/ecommerce/project/model/Users.java
@@ -0,0 +1,38 @@
+package com.ecommerce.project.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "users")
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long userId;
+
+    @NotBlank
+    @Size(max = 20)
+    private String userName;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(max = 120)
+    private String password;
+
+    public Users(String userName, String email, String password) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+}
