590bc8d5df38fac475b69cdf7c1d58ec2f61f731
Haily S <Hailybellestevens@gmail.com>
2024-07-29 11:37:59 -0400

    Modified code to implement cookie based auth

--- src/main/java/com/ecommerce/project/security/response/UserInfoResponse.java
+++ src/main/java/com/ecommerce/project/security/response/UserInfoResponse.java
@@ -4,24 +4,30 @@ import java.util.List;
 
 public class UserInfoResponse {
 
-    private Long Id;
+    private Long id;
     private String jwtToken;
     private String username;
     private List<String> roles;
 
     public UserInfoResponse(Long id, String username, List<String> roles, String jwtToken) {
-        this.Id = id;
+        this.id = id;
         this.username = username;
         this.roles = roles;
         this.jwtToken = jwtToken;
     }
 
+    public UserInfoResponse(Long id, String username, List<String> roles) {
+        this.id = id;
+        this.username = username;
+        this.roles = roles;
+    }
+
     public Long getId() {
-        return Id;
+        return id;
     }
 
     public void setId(Long id) {
-        Id = id;
+        id = id;
     }
 
     public String getJwtToken() {
