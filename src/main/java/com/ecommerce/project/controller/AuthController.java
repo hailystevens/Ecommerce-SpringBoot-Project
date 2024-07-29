e4ab61e28fd9d585181b36215435812e5ad7ea4c
Haily S <Hailybellestevens@gmail.com>
2024-07-29 11:40:21 -0400

    changed jwttoke from null to string

--- src/main/java/com/ecommerce/project/controller/AuthController.java
+++ src/main/java/com/ecommerce/project/controller/AuthController.java
@@ -74,7 +74,7 @@ public class AuthController {
                 .collect(Collectors.toList());
 
         UserInfoResponse response = new UserInfoResponse(userDetails.getId(),
-                userDetails.getUsername(), roles);
+                userDetails.getUsername(), roles, jwtCookie.toString());
 
         return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, jwtCookie.toString()).body(response);
     }
