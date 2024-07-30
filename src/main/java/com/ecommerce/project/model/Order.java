c91ed44d6403dec88cb738e04aa08521dcbe563f
Haily S <Hailybellestevens@gmail.com>
2024-07-30 08:29:19 -0400

    Managing payments

--- src/main/java/com/ecommerce/project/model/Order.java
+++ src/main/java/com/ecommerce/project/model/Order.java
@@ -25,14 +25,14 @@ public class Order {
     @Column(nullable = false)
     private String email;
 
-    @OneToMany(mappedBy = "order", cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+    @OneToMany(mappedBy = "order", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     private List<OrderItem> orderItems = new ArrayList<>();
 
     private LocalDate orderDate;
 
-//    @OneToOne
-//    @JoinColumn(name = "payment_id")
-//    private Payment payment;
+    @OneToOne
+    @JoinColumn(name = "payment_id")
+    private Payment payment;
 
     private Double totalAmount;
     private String orderStatus;
